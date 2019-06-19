411b0d1a09651439ae0de12656c31414bea57645
gilz <gilz@codevalue.net>
2019-06-19 18:22:56 +0300

    read conditions from file

---
+++ ConditionManager.py
@@ -0,0 +1,34 @@
+import re
+
+
+class ConditionManager():
+    def __init__(self):
+        self.conditions = []
+
+
+    def readFromFile(self):
+        conditions = []
+        lineIndex = -1
+        file = open("./condition.txt")
+        line = ""
+        index = 2
+        for tmp in file.readlines():
+            lineIndex += 1
+            startLineWith = re.search(r'(([0-9]+)[.]+(.*))', tmp)
+            index = 2
+            if startLineWith == None:
+                startLineWith = re.search(r'(.*)', tmp)
+                index = 0
+            else:
+                if lineIndex > 0:
+                    self.conditions.append(line)
+                    line = ""
+                    startLineWith = re.search(r'(([0-9]+)[.]+(.*))', tmp)
+                    index = 2
+            line += " " + str(startLineWith.groups()[index])
+            line = line.replace("||", " or ").replace("&&", " and ").replace("=<", "<=").replace("=>", ">=").replace(
+                "\n", "")
+
+        self.conditions.append(line)
+        print("Read Line=%s" % self.conditions)
+        return self.conditions
